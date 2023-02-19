c82c1afa4577900324e2e91f096a0375721bb426
ran-sy <raneem.1999.ta@gmail.com>
2023-02-19 05:36:37 +0200

    almost-done

--- script/script.js
+++ script/script.js
@@ -20,7 +20,7 @@ var user = {
 
 function displayContent() {
   // to check what TAB is the ACTIVE and close the others
-  // chkTabs()
+  chkTabs()
   navMenuBtns.forEach(btn => btn.addEventListener("click", this.chkTabs))
 }
 function chkTabs() {
@@ -28,10 +28,13 @@ function chkTabs() {
     navMenu.forEach(element => {
       let classes = element.getAttribute("class").split(" ")
       if (classes.indexOf("active") != -1) {
-        element.style.display = "block"
+        element.classList.remove('hidden')
+        element.classList.add('d-block')
+        drawProducts(element.getAttribute('id'), element.getAttribute('tab-index'))
       }
       else {
-        element.style.display = "none"
+        element.classList.remove('d-block')
+        element.classList.add('hidden')
       }
     });
   }
@@ -39,28 +42,47 @@ function chkTabs() {
 
 /////////////////////////////////////////////////
 
-class Product {
-  constructor(id, title, quantity, imgUrl, price, unit) {
-    this.id = id,
-      this.title = title,
-      this.quantity = quantity,
-      this.imgUrl = imgUrl,
-      this.price = price,
-      this.unit = unit;
-    getPrice = function () {
-      return unit ? this.price + " " + this.unit : this.price;
-    };
-  }
+function drawProducts(tabId, tabIndex) {
+  console.log(tabId, tabIndex)
+  fetch("./json/products.json")
+    .then((res) => { return res.json(); })
+    .then((data) => {
+      let productTypeList = data[tabIndex][tabId];
+      let mainDiv = document.getElementById(tabId + '-products')
+        productTypeList.forEach((item) => {
+            drawProductCard(item, tabId, productTypeList.length);
+        })
+      // console.log(data[0].dairy[0].id)
+    });
 }
 
 /////////////////////////////////////////////////
 
-function drawProducts() {
-  fetch("./json/products.json")
-    .then((res) => { return res.json(); })
-    .then((data) => console.log(data[0].dairy[0].id));
+function drawProductCard({ ...item }, tabId, length) {
+  let mainDivId = tabId + '-products';
+  if (!(document.getElementById(mainDivId))) {
+    let docDiv = document.createElement("div")
+    docDiv.classList.add("row", "row-cols-2", "row-cols-sm-4", "row-cols-lg-6", "g-3")
+    docDiv.setAttribute("id", mainDivId)
+    document.getElementById(tabId).appendChild(docDiv)
+  }
+  let mainDiv = document.getElementById(mainDivId)
+  if (mainDiv.children.length != length){
+  mainDiv.innerHTML +=
+    `<div class="col">
+      <div class="card">
+        <img src=${item.imgUrl} style="max-height: 100px" class="card-img-top" alt=${item.title}>
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+          <div class="d-flex justify-content-between">
+            <p class="card-text text-dark-red">${item.price}جنيه</p>
+            <p class="card-text">[${item.quantity} ${item.unit}]</p>
+          </div>
+        </div>
+      </div>
+    </div>`;
+  }
 }
-drawProducts()
 
 /////////////////////////////////////////////////
 
@@ -68,8 +90,8 @@ function chkUserLoggedIn() {
   let location = window.location.href.split("/");
   if (user.loggedIn && location[location.length - 1] == "index.html") {
     document.getElementById("helloUser").innerHTML = `${user.name}`;
-    document.getElementById("loginBtn").style.display = "none"
-    document.getElementById("registerBtn").style.display = "none"
+    document.getElementById("loginBtn").classList.add('hidden')
+    document.getElementById("registerBtn").classList.add('hidden')
   }
 }
 logoutBtn.addEventListener("click", () => {
